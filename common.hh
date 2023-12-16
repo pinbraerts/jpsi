b193dc047d8ca31e597111906bfaead00788f8ce
Dmitry Shirshov <shirshov.da@phystech.edu>
2023-12-16 22:47:09 +0300

    move all definitions to common.hh

---
+++ common.hh
@@ -0,0 +1,33 @@
+#pragma once
+
+#include "TVectorDfwd.h"
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include <TVectorD.h>
+#include <TMath.h>
+#include <TFormula.h>
+#include <TF2.h>
+#include <TH2F.h>
+#include <TCanvas.h>
+#include <THStack.h>
+#include <TLegend.h>
+#include <TFile.h>
+#include <TMinuitMinimizer.h>
+#include <Math/Functor.h>
+#include <Math/IntegratorMultiDim.h>
+#include <TStyle.h>
+
+#if __cplusplus < 201703L
+
+namespace std {
+
+template<class T, std::size_t N>
+constexpr std::size_t size(const T (&array)[N]) noexcept
+{
+	return N;
+}
+
+} // namespace std
+
+#endif // __cplusplus < 201703L
--- fit.C
+++ fit.C
@@ -1,33 +1,4 @@
-#define _USE_MATH_DEFINES
-#include <cmath>
-
-#include <TVectorD.h>
-#include <TMath.h>
-#include <TFormula.h>
-#include <TF2.h>
-#include <TH2F.h>
-#include <TCanvas.h>
-#include <THStack.h>
-#include <TLegend.h>
-#include <TFile.h>
-#include <TMinuitMinimizer.h>
-#include <Math/Functor.h>
-#include <Math/IntegratorMultiDim.h>
-#include <TStyle.h>
-
-#if __cplusplus < 201703L
-
-namespace std {
-
-template<class T, std::size_t N>
-constexpr std::size_t size(const T (&array)[N]) noexcept
-{
-	return N;
-}
-
-} // namespace std
-
-#endif // __cplusplus < 201703L
+#include "common.hh"
 
 TH2F* h_jpk;
 TH2F* h_skk;
