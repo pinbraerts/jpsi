0ba111ddc5721addc9a2544625772efb57bf7865
Dmitry Shirshov <shirshov.da@phystech.edu>
2023-12-19 20:40:10 +0300

    Completely refactor jpsi K K

--- fit_jkk.C
+++ fit_jkk.C
@@ -1,14 +1,11 @@
 #include "common.hh"
 
-TH1F* h_jpk;
-TH1F* h_skk;
-TH1F* h_ski;
-TH1F* h_sii;
-TH1F* h_dkk;
-TH1F* h_dki;
-TH1F* h_dii;
-TH1F* h_lpk;
-TH1F* h_lpi;
+using Graph = std::array<TH1F*, 9>;
+using Fit   = std::array<Graph, 3>;
+using Back  = std::array<TF1* , 3>;
+
+Fit h;
+Back f;
 
 double background(double const* x, double const* a) {
 	double const xx = x[0] - 5;
@@ -19,112 +16,106 @@ double background(double const* x, double const* a) {
 	return exp(a[0] + xx * a[1] + xx * xx * a[2]) * exp(-b * b);
 }
 
-TF1 f_back("f_jkk", background, 5.0, 7.0, 7, 1);
-double likelihood(double const* parameters) {
-	f_back.SetParameters(parameters + 8);
-	auto x = h_jpk->GetXaxis();
-	const auto N = x->GetNbins();
+double l_step(Graph& h, TF1* f, size_t i, double const* c) {
+	double const value = h[0]->GetBinContent(i);
+	double const x = h[0]->GetBinCenter(i);
+	double integral = f->Eval(x);
+	for (size_t j = 1; j < std::size(h); ++j) {
+		integral += h[j]->GetBinContent(i) * c[j - 1];
+	}
+	return value * log(integral) - integral;
+}
+
+double likelihood(double const* p) {
 	double L = 0;
-	for (size_t i = 1; i <= N; ++i) {
-		double const value = h_jpk->GetBinContent(i);
-		double const v_skk = h_skk->GetBinContent(i) * parameters[0];
-		double const v_ski = h_ski->GetBinContent(i) * parameters[1];
-		double const v_sii = h_sii->GetBinContent(i) * parameters[2];
-		double const v_dkk = h_dkk->GetBinContent(i) * parameters[3];
-		double const v_dki = h_dki->GetBinContent(i) * parameters[4];
-		double const v_dii = h_dii->GetBinContent(i) * parameters[5];
-		double const v_lpk = h_lpk->GetBinContent(i) * parameters[6];
-		double const v_lpi = h_lpi->GetBinContent(i) * parameters[7];
-		double const xx = x->GetBinCenter(i);
-		double const integral = f_back.Eval(xx) +
-			v_skk + v_ski + v_sii +
-			v_dkk + v_dki + v_dii +
-			v_lpk + v_lpi;
-		L += value * log(integral) - integral;
+	for (size_t j = 0; j < std::size(h); ++j) {
+		f[j]->SetParameters(p + 8 + j * f[j]->GetNpar());
+		auto const N = h[j][0]->GetNbinsX();
+		for (size_t i = 1; i <= N; ++i) {
+			L += l_step(h[j], f[j], i, p);
+		}
 	}
 	return -L;
 }
 
 void fit_jkk() {
-	auto f_jpk = new TFile("filtered/datasets/run2_1quarter.root");
-	auto f_skk = new TFile("filtered/datasets/mc_BS_to_JPSI_K_K.root");
-	auto f_ski = new TFile("filtered/datasets/mc_BS_to_JPSI_K_PI.root");
-	auto f_sii = new TFile("filtered/datasets/mc_BS_to_JPSI_PI_PI.root");
-	auto f_dkk = new TFile("filtered/datasets/mc_BD_to_JPSI_K_K.root");
-	auto f_dki = new TFile("filtered/datasets/mc_BD_to_JPSI_K_PI.root");
-	auto f_dii = new TFile("filtered/datasets/mc_BD_to_JPSI_PI_PI.root");
-	auto f_lpk = new TFile("filtered/datasets/mc_LAMBDA0B_to_JPSI_P_K.root");
-	auto f_lpi = new TFile("filtered/datasets/mc_LAMBDA0B_to_JPSI_P_PI.root");
+	TFile* file[] = {
+		new TFile("filtered/datasets/run2_1quarter.root"),
+		new TFile("filtered/datasets/mc_BS_to_JPSI_K_K.root"),
+		new TFile("filtered/datasets/mc_BS_to_JPSI_K_PI.root"),
+		new TFile("filtered/datasets/mc_BS_to_JPSI_PI_PI.root"),
+		new TFile("filtered/datasets/mc_BD_to_JPSI_K_K.root"),
+		new TFile("filtered/datasets/mc_BD_to_JPSI_PI_PI.root"),
+		new TFile("filtered/datasets/mc_BD_to_JPSI_K_PI.root"),
+		new TFile("filtered/datasets/mc_LAMBDA0B_to_JPSI_P_K.root"),
+		new TFile("filtered/datasets/mc_LAMBDA0B_to_JPSI_P_PI.root"),
+	};
 	auto f_output = new TFile("output/results_jkk.root", "recreate");
 
-	h_jpk = (TH1F*)f_jpk->Get("jkk");
-	h_skk = (TH1F*)f_skk->Get("jkk");
-	h_ski = (TH1F*)f_ski->Get("jkk");
-	h_sii = (TH1F*)f_sii->Get("jkk");
-	h_dkk = (TH1F*)f_dkk->Get("jkk");
-	h_dki = (TH1F*)f_dki->Get("jkk");
-	h_dii = (TH1F*)f_dii->Get("jkk");
-	h_lpk = (TH1F*)f_lpk->Get("jkk");
-	h_lpi = (TH1F*)f_lpi->Get("jkk");
-	h_skk->SetName("skk");
-	h_ski->SetName("ski");
-	h_sii->SetName("sii");
-	h_dkk->SetName("dkk");
-	h_dki->SetName("dki");
-	h_dii->SetName("dii");
-	h_lpk->SetName("lpk");
-	h_lpi->SetName("lpi");
+	char const* label[] = {
+		"Data",
+		// "Combinatorial BG",
+		"B_s \\rightarrow J/\\psi K K",
+		"B_s \\rightarrow J/\\psi K \\pi",
+		"B_s \\rightarrow J/\\psi \\pi \\pi",
+		"B_d \\rightarrow J/\\psi K K",
+		"B_d \\rightarrow J/\\psi \\pi \\pi",
+		"B_d \\rightarrow J/\\psi K \\pi",
+		"\\Lambda^0_b \\rightarrow J/\\psi p K",
+		"\\Lambda^0_b \\rightarrow J/\\psi p \\pi",
+	};
+	std::string const obj_name[] {
+		"jkk", "jki", "jik",
+	};
 
-	// h_jpk->Scale(1 / h_jpk->Integral());
-	h_skk->Scale(1 / h_skk->Integral());
-	h_ski->Scale(1 / h_ski->Integral());
-	h_sii->Scale(1 / h_sii->Integral());
-	h_dkk->Scale(1 / h_dkk->Integral());
-	h_dki->Scale(1 / h_dki->Integral());
-	h_dii->Scale(1 / h_dii->Integral());
-	h_lpk->Scale(1 / h_lpk->Integral());
-	h_lpi->Scale(1 / h_lpi->Integral());
+	for (size_t j = 0; j < std::size(h); ++j) {
+		f[j] = new TF1((obj_name[j] + "_comb_bg").c_str(), background, 5.0, 7.0, 7, 1);
+		for (size_t i = 0; i < std::size(h[0]); ++i) {
+			h[j][i] = (TH1F*)file[i]->Get(obj_name[j].c_str());
+			h[j][i]->SetName((obj_name[j] + label[i]).c_str());
+			if (i > 0) {
+				auto const integral = h[j][i]->Integral();
+				h[j][i]->Scale(1 / integral);
+			}
+		}
+	}
 
 	TMinuitMinimizer m;
 	m.SetMaxFunctionCalls(1e7);
 	m.SetMaxIterations(1e6);
-	m.SetTolerance(1e-3);
+	m.SetTolerance(1e-5);
 	m.SetErrorDef(0.5);
 	m.SetPrintLevel(1);
 
 	double step[]  {
 		1, 1, 1, 1, 1, 1, 1, 1,
-		1e-4, 1e-4, 1e-4,
-		1e-4, 1e-4, 1e-4, 1e-4,
-		// 1e-1, 1e-1,
+		1e-4, 1e-4, 1e-4, 1e-4, 1e-4, 1e-4, 1e-4,
+		1e-4, 1e-4, 1e-4, 1e-4, 1e-4, 1e-4, 1e-4,
+		1e-4, 1e-4, 1e-4, 1e-4, 1e-4, 1e-4, 1e-4,
 	};
 	double start[] {
-		1008, 210, 3116, 1, 11352, 1665, 3060, 1,
-		9.6, 2, -19.56,
-		-0.02, 0.02, 0.11, 0.22,
-		// 1, 1,
+		1008, 210, 3116, 2, 11352, 1665, 3060, 2,
+		8.3, -6, 2.35, -2.68, 9.8, 2.2, 7.46,
+		7.29, 10, 700, -0.09, -12, 0.45, 0.002,
+		7.2, 2, -19.56, -0.02, 0.02, 0.11, 0.22,
 	};
 	char const* name[] {
-		"c_skk",
-		"c_ski",
-		"c_sii",
-		"c_dkk",
-		"c_dki",
-		"c_dii",
-		"c_lpk",
-		"c_lpi",
-		"a", "a_x", "a_x^2",
-		"b", "bx", "c", "cx",
-		// "x", "x^",
+		"c_skk", "c_ski", "c_sii", "c_dkk", "c_dki", "c_dii", "c_lpk", "c_lpi",
+		"jkk_a", "jkk_a_x", "jkk_a_x^2", "jkk_b", "jkk_bx", "jkk_c", "jkk_cx",
+		"jki_a", "jki_a_x", "jki_a_x^2", "jki_b", "jki_bx", "jki_c", "jki_cx",
+		"jik_a", "jik_a_x", "jik_a_x^2", "jik_b", "jik_bx", "jik_c", "jik_cx",
 	};
 
-	ROOT::Math::Functor f(&likelihood, std::size(start));
-	m.SetFunction(f);
+	ROOT::Math::Functor functor(&likelihood, std::size(start));
+	m.SetFunction(functor);
 	for (size_t i = 0; i < std::size(start); ++i) {
 		if (i <= 7) {
-			m.SetLimitedVariable(i, name[i], start[i], step[i], 0e0, 1e6);
-			// m.SetFixedVariable(i, name[i], 2 * start[i]);
+			m.SetLimitedVariable(i, name[i], start[i], step[i], 0, 1e6);
+			// m.SetFixedVariable(i, name[i], start[i]);
 		}
+		// else if (i < 22) {
+			// m.SetFixedVariable(i, name[i], start[i]);
+		// }
 		else {
 			m.SetVariable(i, name[i], start[i], step[i]);
 		}
@@ -136,99 +127,74 @@ void fit_jkk() {
 		result = start;
 	}
 	else {
-		TVectorD errs(f.NDim(), m.Errors());
+		TVectorD errs(functor.NDim(), m.Errors());
 		errs.Write("errors");
 		TVectorD params(std::size(start), result);
 		params.Write("constants");
 	}
-	f_back.SetParameters(result + 8);
-	f_back.Write("f_background_combinatorial");
 
-	h_skk->Scale(result[0]);
-	h_ski->Scale(result[1]);
-	h_sii->Scale(result[2]);
-	h_dkk->Scale(result[3]);
-	h_dki->Scale(result[4]);
-	h_dii->Scale(result[5]);
-	h_lpk->Scale(result[6]);
-	h_lpi->Scale(result[7]);
-
-	auto back = (TH1F*)h_skk->Clone("combinatorial BG");
-	auto full = (TH1F*)h_skk->Clone("full BG");
-	auto x = full->GetXaxis();
-	auto const N = x->GetNbins();
-	full->Clear();
-	back->Clear();
-	for (size_t i = 1; i <= N; ++i) {
-		auto const xx = x->GetBinCenter(i);
-		double const value = f_back.Eval(xx);
-		double const v_skk = h_skk->GetBinContent(i);
-		double const v_ski = h_ski->GetBinContent(i);
-		double const v_sii = h_sii->GetBinContent(i);
-		double const v_dkk = h_dkk->GetBinContent(i);
-		double const v_dki = h_dki->GetBinContent(i);
-		double const v_dii = h_dii->GetBinContent(i);
-		double const v_lpk = h_lpk->GetBinContent(i);
-		double const v_lpi = h_lpi->GetBinContent(i);
-		double const integral = value + (
-			v_skk + v_ski + v_sii +
-			v_dkk + v_dki + v_dii +
-			v_lpk + v_lpi
-		);
-		back->SetBinContent(i, value);
-		full->SetBinContent(i, integral);
-	}
-	full->Write("background");
-	back->Write("h_background_combinatorial");
-
-	TH1F* histograms[] = {
-		h_jpk, back,
-		h_skk, h_ski, h_sii,
-		h_dkk, h_dii, h_dki,
-		h_lpk, h_lpi
-	};
-	char const* labels[] = {
-		"Data",
-		"Combinatorial BG",
-		"B_s \\rightarrow J/\\psi K K",
-		"B_s \\rightarrow J/\\psi K \\pi",
-		"B_s \\rightarrow J/\\psi \\pi \\pi",
-		"B_d \\rightarrow J/\\psi K K",
-		"B_d \\rightarrow J/\\psi \\pi \\pi",
-		"B_d \\rightarrow J/\\psi K \\pi",
-		"\\Lambda^0_b \\rightarrow J/\\psi p K",
-		"\\Lambda^0_b \\rightarrow J/\\psi p \\pi",
-	};
-
-	gStyle->SetOptTitle(0);
-	// gStyle->SetOptStat(0);
-	auto canvas = new TCanvas();
-	auto legend = new TLegend(0.6, 0.25, 0.9, 0.9);
-	auto stack = new THStack("stack", "stack");
-	double chi2 = 0;
-	for (size_t i = 0; i < std::size(histograms); ++i) {
-		auto x = histograms[i];
-		x->SetFillStyle(3001);
-		if (i > 0) {
-			stack->Add(x);
+	TH1F* back[std::size(h)];
+	TH1F* full[std::size(h)];
+	for (size_t j = 0; j < std::size(h); ++j) {
+		back[j] = (TH1F*)h[j][0]->Clone((obj_name[j] + "back").c_str());
+		back[j]->Clear();
+		full[j] = (TH1F*)h[j][0]->Clone((obj_name[j] + "full").c_str());
+		full[j]->Clear();
+		f[j]->SetParameters(result + 8 + j * f[j]->GetNpar());
+		auto const N = h[j][0]->GetNbinsX();
+		for (size_t k = 1; k < std::size(h[0]); ++k) {
+			h[j][k]->Scale(result[k - 1]);
 		}
-		else {
-			x->SetFillColor(kRed);
-			x->SetLineColor(kRed);
-			x->Draw("E");
-			chi2 = full->Chi2Test(x);
+		for (size_t i = 1; i <= N; ++i) {
+			auto const x = h[j][0]->GetBinCenter(i);
+			auto value = f[j]->Eval(x);
+			back[j]->SetBinContent(i, value);
+			for (size_t k = 1; k < std::size(h[0]); ++k) {
+				value += h[j][k]->GetBinContent(i);
+			}
+			full[j]->SetBinContent(i, value);
 		}
-		x->Write(labels[i]);
-		legend->AddEntry(x, labels[i]);
 	}
 
-	TVectorD chi2s(1, &chi2);
+	gStyle->SetOptTitle(1);
+	gStyle->SetOptStat(0);
+	std::vector<double> chi2;
+	for (size_t j = 0; j < std::size(h); ++j) {
+		auto c = new TCanvas(obj_name[j].c_str(), obj_name[j].c_str());
+		auto s = new THStack();
+		auto l = new TLegend(0.6, 0.3, 0.9, 0.9);
+		s->Add(back[j]);
+		l->AddEntry(back[j], "Combinatorial BG");
+		back[j]->Write();
+		full[j]->Write();
+		for (size_t k = 0; k < std::size(h[0]); ++k) {
+			auto x = h[j][k];
+			x->SetFillStyle(3001);
+			if (k == 0) {
+				x->SetFillColor(kRed);
+				x->SetLineColor(kRed);
+				x->Draw("E");
+				x->SetTitle(obj_name[j].c_str());
+				chi2.emplace_back(full[j]->Chi2Test(x));
+			}
+			else {
+				s->Add(x);
+			}
+			x->Write();
+			l->AddEntry(x, label[k]);
+		}
+
+		s->Draw("noclear same hist pfc");
+		l->Draw();
+		c->SetTitle(obj_name[j].c_str());
+		c->Draw();
+		c->Write();
+		c->SaveAs(("output/" + obj_name[j] + ".eps").c_str());
+		c->SaveAs(("output/" + obj_name[j] + ".png").c_str());
+
+	}
+
+	TVectorD chi2s(chi2.size(), chi2.data());
 	chi2s.Write("chi2s");
-	std::cout << "chi2: " << chi2 << std::endl;
-
-	stack->Draw("noclear same hist pfc");
-	legend->Draw();
-	canvas->Draw();
-	canvas->Write("plot");
 
 }
