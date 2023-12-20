fde85c607cde08a29197a6ef709200c3c72436be
Dmitry Shirshov <shirshov.da@phystech.edu>
2023-12-20 23:49:39 +0300

    fix fit.C(false)

--- fit.C
+++ fit.C
@@ -102,7 +102,7 @@ void fit(bool fix = false) {
 	double step[]  {
 		1e1, 1e1, 1e1, 1e1, 1e1, 1e1, 1e1, 1e1,
 			   1,			1,	  1,
-		1e-2, 1e-4, 1e-4, 1e-4,
+		1e-2, 1e-2, 1e-2, 1e-2,
 		// 1e-1, 1e-1,
 	};
 	double start[] {
